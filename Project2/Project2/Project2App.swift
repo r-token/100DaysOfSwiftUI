9807ef32d6f3a0a1caaca13f75ad40b1303e39d0
r-token <ryan@storyboard.fm>
2021-07-02 09:33:02 -0500

    Added the main parts of Project 2 - Guess the Flag

---
+++ Project2/Project2/Project2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project2App.swift
+//  Project2
+//
+//  Created by Ryan Token on 7/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
