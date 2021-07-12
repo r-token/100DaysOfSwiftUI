45fbef66b20757ad06c3ece6648d27a33b48a08c
Ryan Token <ryantoken13@gmail.com>
2021-07-12 08:52:48 -0500

    Began Project6 - Animations

---
+++ Project6/Project6/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Project6
+//
+//  Created by Ryan Token on 7/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var animationAmount = 0.0
+    
+    var body: some View {
+        Button("Tap Me") {
+            withAnimation {
+                animationAmount += 180
+            }
+        }
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project6/Project6/Project6App.swift
@@ -0,0 +1,17 @@
+//
+//  Project6App.swift
+//  Project6
+//
+//  Created by Ryan Token on 7/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Project6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
