b405ba969ed9f7b2bd26ead7b167eed50fd1eadf
r-token <ryan@storyboard.fm>
2021-07-15 09:51:16 -0500

    Finished basic options layout and initial logic for Project6.5 - Edutainment

--- Project6.5/Project6.5/ButtonModifier.swift
+++ Project6.5/Project6.5/ButtonModifier.swift
@@ -10,16 +10,32 @@ import SwiftUI
 struct ButtonModifier: ViewModifier {
 	func body(content: Content) -> some View {
 		content
-			.frame(width: 50, height: 50)
-			.background(Color.white)
 			.clipShape(RoundedRectangle(cornerRadius: 5.0))
 			.font(.headline)
 			.shadow(radius: 10)
 	}
 }
 
+struct StartButtonModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(width: 200, height: 50)
+            .background(Color.white)
+            .foregroundColor(.purple)
+            .font(.title2)
+            .clipShape(Capsule())
+            .shadow(radius: 20)
+    }
+}
+
 extension View {
 	func buttonStyle() -> some View {
 		return modifier(ButtonModifier())
 	}
+}
+
+extension View {
+    func startButtonStyle() -> some View {
+        return modifier(StartButtonModifier())
+    }
 }
--- Project6.5/Project6.5/ContentView.swift
+++ Project6.5/Project6.5/ContentView.swift
@@ -8,100 +8,145 @@
 import SwiftUI
 
 struct ContentView: View {
-	@State private var selectedTables = [Int]()
-	@State private var numberOfQuestions = 5
-	
-	let tableOptions = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
-	let numQuestionOptions = ["5", "10", "20", "All"]
+    @State private var selectedTables = [1]
+    @State private var numberOfQuestions = "5"
+    
+    @State private var showSetupView = true
+    
+    let tableOptions = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+    let numQuestionOptions = ["5", "10", "20", "All"]
 	
     var body: some View {
 		NavigationView {
 			ZStack {
 				Color.purple
 					.edgesIgnoringSafeArea(.all)
-				Group {
-					VStack(spacing: 20) {
-						Spacer()
-						Spacer()
-						Text("Which times tables do you want to practice?")
-							.fixedSize(horizontal: false, vertical: true)
-							.font(.headline)
-							.foregroundColor(.white)
-							.frame(height: 10, alignment: .bottom)
-						
-						VStack(spacing: 25) {
-							HStack(spacing: 25) {
-								ForEach(0..<4) { option in
-									Button(action: {
-										// button pressed
-									}, label: {
-										Text("\(tableOptions[option])s")
-											.buttonStyle()
-									})
-								}
-							}
-							HStack(spacing: 25) {
-								ForEach(4..<8) { option in
-									Button(action: {
-										// button pressed
-									}, label: {
-										Text("\(tableOptions[option])s")
-											.buttonStyle()
-									})
-								}
-							}
-							HStack(spacing: 25) {
-								ForEach(8..<12) { option in
-									Button(action: {
-										// button pressed
-									}, label: {
-										Text("\(tableOptions[option])s")
-											.buttonStyle()
-									})
-								}
-							}
-						}
-						
-						VStack(alignment: .center, spacing: 25) {
-							Spacer()
-							Text("How many questions should we ask?")
-								.font(.headline)
-								.foregroundColor(.white)
-								.frame(height: 10, alignment: .bottom)
-							
-							HStack(spacing: 25) {
-								ForEach(numQuestionOptions, id: \.self) { option in
-									Button(action: {
-										// button pressed
-									}, label: {
-										Text(option)
-											.buttonStyle()
-									})
-								}
-							}
-						}
-						
-						Spacer()
-						Spacer()
-						
-						Button(action: {
-							// start game
-						}, label: {
-							Text("Start!")
-								.frame(width: 200, height: 50)
-								.background(Color.white)
-								.foregroundColor(.pink)
-								.font(.title2)
-								.clipShape(Capsule())
-								.shadow(radius: 10)
-						})
-						
-						Spacer()
-					}
-					.padding()
-				}
-				.navigationBarTitle(Text("Edutainment"))
+                    
+                if showSetupView {
+                    Group {
+                        VStack(spacing: 20) {
+                            Spacer()
+                            Spacer()
+                            Text("Which times tables do you want to practice?")
+                                .fixedSize(horizontal: false, vertical: true)
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .frame(height: 10, alignment: .bottom)
+                            
+                            VStack(spacing: 25) {
+                                HStack(spacing: 25) {
+                                    ForEach(0..<4) { option in
+                                        Button(action: {
+                                            if selectedTables.contains(option+1) {
+                                                if let itemToRemoveIndex = selectedTables.firstIndex(of: option+1) {
+                                                    selectedTables.remove(at: itemToRemoveIndex)
+                                                }
+                                            } else {
+                                                selectedTables.append(option+1)
+                                            }
+                                            print(selectedTables)
+                                        }, label: {
+                                            Text("\(tableOptions[option])s")
+                                                .frame(width: 50, height: 50)
+                                                .background(selectedTables.contains(option+1) ? Color.blue : Color.white)
+                                                .foregroundColor(selectedTables.contains(option+1) ? .white : .blue)
+                                                .buttonStyle()
+                                        })
+                                    }
+                                }
+                                HStack(spacing: 25) {
+                                    ForEach(4..<8) { option in
+                                        Button(action: {
+                                            if selectedTables.contains(option+1) {
+                                                if let itemToRemoveIndex = selectedTables.firstIndex(of: option+1) {
+                                                    selectedTables.remove(at: itemToRemoveIndex)
+                                                }
+                                            } else {
+                                                selectedTables.append(option+1)
+                                            }
+                                            print(selectedTables)
+                                        }, label: {
+                                            Text("\(tableOptions[option])s")
+                                                .frame(width: 50, height: 50)
+                                                .background(selectedTables.contains(option+1) ? Color.blue : Color.white)
+                                                .foregroundColor(selectedTables.contains(option+1) ? .white : .blue)
+                                                .buttonStyle()
+                                        })
+                                    }
+                                }
+                                HStack(spacing: 25) {
+                                    ForEach(8..<12) { option in
+                                        Button(action: {
+                                            if selectedTables.contains(option+1) {
+                                                if let itemToRemoveIndex = selectedTables.firstIndex(of: option+1) {
+                                                    selectedTables.remove(at: itemToRemoveIndex)
+                                                }
+                                            } else {
+                                                selectedTables.append(option+1)
+                                            }
+                                            print(selectedTables)
+                                        }, label: {
+                                            Text("\(tableOptions[option])s")
+                                                .frame(width: 50, height: 50)
+                                                .background(selectedTables.contains(option+1) ? Color.blue : Color.white)
+                                                .foregroundColor(selectedTables.contains(option+1) ? .white : .blue)
+                                                .buttonStyle()
+                                        })
+                                    }
+                                }
+                            }
+                            
+                            VStack(alignment: .center, spacing: 25) {
+                                Spacer()
+                                Text("How many questions should we ask?")
+                                    .font(.headline)
+                                    .foregroundColor(.white)
+                                    .frame(height: 10, alignment: .bottom)
+                                
+                                HStack(spacing: 25) {
+                                    ForEach(numQuestionOptions, id: \.self) { option in
+                                        Button(action: {
+                                            numberOfQuestions = option
+                                            print(numberOfQuestions)
+                                        }, label: {
+                                            Text(option)
+                                                .frame(width: 50, height: 50)
+                                                .background(option == numberOfQuestions ? Color.blue : Color.white)
+                                                .foregroundColor(option == numberOfQuestions ? .white : .blue)
+                                                .buttonStyle()
+                                        })
+                                    }
+                                }
+                            }
+                            
+                            Spacer()
+                            Spacer()
+                            
+                            Button(action: {
+                                showSetupView = false
+                            }, label: {
+                                Text("Start!")
+                                    .startButtonStyle()
+                            })
+                            
+                            Spacer()
+                        }
+                        .padding()
+                    }
+                } else { // show game view
+                    Group {
+                        VStack {
+                            Button(action: {
+                                showSetupView = true
+                            }, label: {
+                                Text("Back to Options")
+                                    .startButtonStyle()
+                            })
+                        }
+                    }
+                }
 			}
+            .navigationBarTitle(Text("Edutainment"))
 		}
     }
 }
