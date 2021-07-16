dc997a8800286ce101c580581c76d7a49fb729a1
Ryan Token <ryantoken13@gmail.com>
2021-07-16 09:07:17 -0500

    Learned how to show/hide sheets and pass params into them

--- Project7-iExpense/Project7-iExpense/ContentView.swift
+++ Project7-iExpense/Project7-iExpense/ContentView.swift
@@ -7,29 +7,37 @@
 
 import SwiftUI
 
-class User: ObservableObject {
-    @Published var firstName = "Bilbo"
-    @Published var lastName = "Baggins"
-}
-
 struct ContentView: View {
-    @ObservedObject var user = User()
+    @State private var showingSheet = false
     
     var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName)")
-            
-            TextField("First Name", text: $user.firstName)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            TextField("Last Name", text: $user.lastName)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
+        Button("Show Sheet") {
+            showingSheet.toggle()
         }
-        .padding()
+        .sheet(isPresented: $showingSheet, content: {
+            SecondView(name: "Ryan")
+        })
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct SecondView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    var name: String
+    
+    var body: some View {
+        VStack {
+            Text("Hi \(name)")
+            
+            Button("Dismiss") {
+                self.presentationMode.wrappedValue.dismiss()
+            }
+        }
     }
 }
