99963e7ea0c0a318c3d2ff14d1237eb7422fcedd
Ryan Token <ryantoken13@gmail.com>
2021-07-16 08:39:35 -0500

    Added names to the project folders

---
+++ Project7-iExpense/Project7-iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project7-iExpense
+//
+//  Created by Ryan Token on 7/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project7-iExpense/Project7-iExpense/Project7_iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project7_iExpenseApp.swift
+//  Project7-iExpense
+//
+//  Created by Ryan Token on 7/16/21.
+//
+
+import SwiftUI
+
+@main
+struct Project7_iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
