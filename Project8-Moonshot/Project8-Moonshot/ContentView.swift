f21691a840bb06ac78dca7c2d0dfea3a73b7e826
Ryan Token <ryantoken13@gmail.com>
2021-07-18 18:44:31 -0500

    Completed the intro to Project8 - Moonshot, and played with more complex Codable data

--- Project8-Moonshot/Project8-Moonshot/ContentView.swift
+++ Project8-Moonshot/Project8-Moonshot/ContentView.swift
@@ -9,17 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationView {
-            List(0..<100) { row in
-                NavigationLink(
-                    destination: Text("Detail \(row)"),
-                    label: {
-                        Text("Row \(row)")
-                    })
+        Button("Decode JSON") {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashville"
+                }
             }
-            .navigationBarTitle("SwiftUI")
+            """
+
+            let data = Data(input.utf8)
+            let decoder = JSONDecoder()
+            if let user = try? decoder.decode(User.self, from: data) {
+                print(user.name)
+                print(user.address.street)
+            }
         }
     }
+}
+
+struct User: Codable {
+    var name: String
+    var address: Address
+}
+
+struct Address: Codable {
+    var street: String
+    var city: String
 }
 
 struct ContentView_Previews: PreviewProvider {
