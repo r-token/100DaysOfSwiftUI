98034e555ba2cc310ab7c3f239882fa531e49d46
Ryan Token <ryantoken13@gmail.com>
2021-07-22 22:51:16 -0500

    Began Project9 - Drawing

---
+++ Project9-Drawing/Project9-Drawing/ContentView.swift
@@ -0,0 +1,67 @@
+//
+//  ContentView.swift
+//  Project9-Drawing
+//
+//  Created by Ryan Token on 7/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Triangle()
+                .stroke(Color.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+                .frame(width: 300, height: 300)
+            
+            Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
+                .strokeBorder(Color.blue, lineWidth: 40)
+            
+            Circle()
+                .strokeBorder(Color.blue, lineWidth: 40)
+        }
+    }
+}
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+
+        return path
+    }
+}
+
+struct Arc: InsettableShape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+    var insetAmount: CGFloat = 0
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
+    }
+
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+
+        return path
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project9-Drawing/Project9-Drawing/Project9_DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project9_DrawingApp.swift
+//  Project9-Drawing
+//
+//  Created by Ryan Token on 7/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Project9_DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
