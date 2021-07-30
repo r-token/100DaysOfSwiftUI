2d29c2ffbee8571dc614846441242328f464b6bb
r-token <ryan@storyboard.fm>
2021-07-30 16:59:41 -0500

    Completed the basics of Project10 - CupcakeCorner

--- Project10-CupcakeCorner/Project10-CupcakeCorner/CheckoutView.swift
+++ Project10-CupcakeCorner/Project10-CupcakeCorner/CheckoutView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CheckoutView: View {
     @ObservedObject var order: Order
+    @State private var confirmationMessage = ""
+    @State private var isShowingConfirmation = false
     
     var body: some View {
         GeometryReader { geo in
@@ -23,13 +25,48 @@ struct CheckoutView: View {
                         .font(.title)
 
                     Button("Place Order") {
-                        // place the order
+                        placeOrder()
                     }
                     .padding()
                 }
             }
         }
+        .alert(isPresented: $isShowingConfirmation) {
+            Alert(title: Text("Thank you!"), message: Text(confirmationMessage), dismissButton: .default(Text("OK")))
+        }
+        
         .navigationBarTitle("Check out", displayMode: .inline)
+    }
+    
+    func placeOrder() {
+        // 1. Convert our current order object into some JSON data that can be sent
+        guard let encodedOrder = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        // 2. Prepare a URLRequest to send our encoded data as JSON
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/JSON", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = encodedOrder
+        
+        // 3. Run that request and process the response
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                print("No data in response: \(error?.localizedDescription ?? "Unknown error").")
+                return
+            }
+            
+            if let decodedOrder = try? JSONDecoder().decode(Order.self, from: data) {
+                confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+                isShowingConfirmation = true
+            } else {
+                confirmationMessage = "Error placing order! Err: \(error?.localizedDescription ?? "Unknown error")"
+                print("Invalid response from server")
+            }
+        }.resume()
     }
 }
 
--- Project10-CupcakeCorner/Project10-CupcakeCorner/Order.swift
+++ Project10-CupcakeCorner/Project10-CupcakeCorner/Order.swift
@@ -7,8 +7,46 @@
 
 import SwiftUI
 
-class Order: ObservableObject {
+final class Order: ObservableObject, Codable {
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    enum CodingKeys: CodingKey {
+        case type, quantity, extraFrosting, addSprinkles, name, streetAddress, city, zip
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(type, forKey: .type)
+        try container.encode(quantity, forKey: .quantity)
+        
+        try container.encode(extraFrosting, forKey: .extraFrosting)
+        try container.encode(addSprinkles, forKey: .addSprinkles)
+
+        try container.encode(name, forKey: .name)
+        try container.encode(streetAddress, forKey: .streetAddress)
+        try container.encode(city, forKey: .city)
+        try container.encode(zip, forKey: .zip)
+    }
+    
+    // default initializer so we can create an order without needing to pass one in
+    init() { }
+    
+    // enables decoding an order from JSON back into our Swift types
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        type = try container.decode(Int.self, forKey: .type)
+        quantity = try container.decode(Int.self, forKey: .quantity)
+
+        extraFrosting = try container.decode(Bool.self, forKey: .extraFrosting)
+        addSprinkles = try container.decode(Bool.self, forKey: .addSprinkles)
+
+        name = try container.decode(String.self, forKey: .name)
+        streetAddress = try container.decode(String.self, forKey: .streetAddress)
+        city = try container.decode(String.self, forKey: .city)
+        zip = try container.decode(String.self, forKey: .zip)
+    }
     
     @Published var type = 0
     @Published var quantity = 3
@@ -21,6 +59,7 @@ class Order: ObservableObject {
             }
         }
     }
+    
     @Published var extraFrosting = false
     @Published var addSprinkles = false
     
