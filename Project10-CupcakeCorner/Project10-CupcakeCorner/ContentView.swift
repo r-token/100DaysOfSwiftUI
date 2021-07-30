389ba3b1f630d1e0fb100536d17f376db39a3059
Ryan Token <ryantoken13@gmail.com>
2021-07-29 22:06:26 -0500

    Completed Part 1 of Project10 - CupcakeCorner & learned about validating and disabling sections of forms

--- Project10-CupcakeCorner/Project10-CupcakeCorner/ContentView.swift
+++ Project10-CupcakeCorner/Project10-CupcakeCorner/ContentView.swift
@@ -8,55 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var results = [Result]()
+    @State private var username = ""
+    @State private var email = ""
     
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
+    }
+
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
             }
-        }
-        .onAppear(perform: loadData)
-    }
-    
-    func loadData() {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        let request = URLRequest(url: url)
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data {
-                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                    // we have good data – go back to the main thread
-                    DispatchQueue.main.async {
-                        // update our UI
-                        results = decodedResponse.results
-                    }
 
-                    // everything is good, so we can exit
-                    return
+            Section {
+                Button("Create account") {
+                    print("Creating account…")
                 }
+                .disabled(disableForm)
             }
 
-            // if we're still here it means there was a problem
-            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
-        }.resume()
+        }
     }
-}
-
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
 }
 
 struct ContentView_Previews: PreviewProvider {
