423e08d878eb7c3df3e5a36d2bcd6e9a68619380
Ryan Token <ryantoken13@gmail.com>
2021-07-29 21:46:33 -0500

    Began Project10 - Cupcake Corner; and learned how to add Codable conformance for @Published properties

---
+++ Project10-CupcakeCorner/Project10-CupcakeCorner/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Project10-CupcakeCorner
+//
+//  Created by Ryan Token on 7/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+class User: ObservableObject, Codable {
+    @Published var name = "Paul Hudson"
+    
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    
+    // required init => if you subclass this, you need to provide your own initializer
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+}
---
+++ Project10-CupcakeCorner/Project10-CupcakeCorner/Project10_CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project10_CupcakeCornerApp.swift
+//  Project10-CupcakeCorner
+//
+//  Created by Ryan Token on 7/29/21.
+//
+
+import SwiftUI
+
+@main
+struct Project10_CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
