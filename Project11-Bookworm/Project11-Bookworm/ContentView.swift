b6c54adee35e92fbbe87830720996ed2a9f490b4
Ryan Token <ryantoken13@gmail.com>
2021-07-31 16:32:08 -0500

    Completed Part 1 of Project11 - Bookworm, and reset the project back to its original state

--- Project11-Bookworm/Project11-Bookworm/ContentView.swift
+++ Project11-Bookworm/Project11-Bookworm/ContentView.swift
@@ -9,48 +9,35 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-	@State private var rememberMe = false
-	
     @Environment(\.managedObjectContext) var moc
-	@Environment(\.horizontalSizeClass) var sizeClass
 
-	@FetchRequest(entity: Student.entity(), sortDescriptors: []) var students: FetchedResults<Student>
+//	@FetchRequest(entity: Student.entity(), sortDescriptors: []) var students: FetchedResults<Student>
     // private var items: FetchedResults<Item>
 
     var body: some View {
-		VStack {
-			List {
-				ForEach(students, id: \.id) { student in
-					Text(student.name ?? "Unknown")
-				}
-			}
-			
-			Button("Add Student") {
-				addStudent()
-			}
-		}
+		Text("Hello, world.")
     }
 
-    private func addStudent() {
-		let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
-		let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
-
-		let chosenFirstName = firstNames.randomElement()!
-		let chosenLastName = lastNames.randomElement()!
-		
-        withAnimation {
-            let student = Student(context: moc)
-			student.id = UUID()
-			student.name = "\(chosenFirstName) \(chosenLastName)"
-
-            do {
-                try moc.save()
-            } catch {
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+//    private func addStudent() {
+//		let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+//		let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+//
+//		let chosenFirstName = firstNames.randomElement()!
+//		let chosenLastName = lastNames.randomElement()!
+//
+//        withAnimation {
+//            let student = Student(context: moc)
+//			student.id = UUID()
+//			student.name = "\(chosenFirstName) \(chosenLastName)"
+//
+//            do {
+//                try moc.save()
+//            } catch {
+//                let nsError = error as NSError
+//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+//            }
+//        }
+//    }
 
 //    private func deleteItems(offsets: IndexSet) {
 //        withAnimation {
@@ -66,25 +53,6 @@ struct ContentView: View {
 //            }
 //        }
 //    }
-}
-
-struct PushButton: View {
-	let title: String
-	@Binding var isOn: Bool
-
-	var onColors = [Color.red, Color.yellow]
-	var offColors = [Color(white: 0.6), Color(white: 0.4)]
-
-	var body: some View {
-		Button(title) {
-			self.isOn.toggle()
-		}
-		.padding()
-		.background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
-		.foregroundColor(.white)
-		.clipShape(Capsule())
-		.shadow(radius: isOn ? 0 : 5)
-	}
 }
 
 private let itemFormatter: DateFormatter = {
