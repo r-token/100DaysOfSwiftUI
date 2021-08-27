82a6a53288dcccf5dcfe7eb3196f11fb1a97200a
Ryan Token <ryantoken13@gmail.com>
2021-08-26 21:40:35 -0500

    Began Project15.5 - PhotoFriends

---
+++ Project15.5-PhotoFriends/Project15.5-PhotoFriends/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project15.5-PhotoFriends
+//
+//  Created by Ryan Token on 8/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project15.5-PhotoFriends/Project15.5-PhotoFriends/Project15_5_PhotoFriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project15_5_PhotoFriendsApp.swift
+//  Project15.5-PhotoFriends
+//
+//  Created by Ryan Token on 8/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Project15_5_PhotoFriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
