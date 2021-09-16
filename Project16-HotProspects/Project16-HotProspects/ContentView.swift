e925653dd5946664ca7c8da5743ba148561f80f2
Ryan Token <ryantoken13@gmail.com>
2021-09-16 17:12:03 -0500

    Learned how to control image interpolation

--- Project16-HotProspects/Project16-HotProspects/ContentView.swift
+++ Project16-HotProspects/Project16-HotProspects/ContentView.swift
@@ -7,36 +7,17 @@
 
 import SwiftUI
 
-enum NetworkError: Error {
-    case badURL, requestFailed, unknown
-}
-
 struct ContentView: View {
-    @ObservedObject var updater = DelayedUpdater()
-    
     var body: some View {
-        Text("Value is \(updater.value)")
+        Image("example")
+            .interpolation(.none)
+            .resizable()
+            .scaledToFit()
+            .frame(maxHeight: .infinity)
+            .background(Color.black)
+            .edgesIgnoringSafeArea(.all)
     }
 }
-
-class DelayedUpdater: ObservableObject {
-    var value = 0 {
-        // this works just like @Published does, but now we can add extra functionality within willSet like logging or calling another method
-        willSet {
-            objectWillChange.send()
-            print("Going up!")
-        }
-    }
-
-    init() {
-        for i in 1...10 {
-            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
-                self.value += 1
-            }
-        }
-    }
-}
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
