4c30dc759d0d3b307cdaf01f764703799fc20744
Ryan Token <ryantoken13@gmail.com>
2021-09-16 17:08:11 -0500

    Learned how to manually publish ObservableObject changes

--- Project16-HotProspects/Project16-HotProspects/ContentView.swift
+++ Project16-HotProspects/Project16-HotProspects/ContentView.swift
@@ -12,50 +12,28 @@ enum NetworkError: Error {
 }
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world")
-            .onAppear {
-                fetchData(from: "https://www.apple.com") { result in
-                    switch result {
-                    case .success(let str):
-                        print(str)
-                    case .failure(let error):
-                        switch error {
-                        case .badURL:
-                            print("Bad URL")
-                        case .requestFailed:
-                            print("Network problems")
-                        case .unknown:
-                            print("Unknown error")
-                        }
-                    }
-                }
-            }
-    }
+    @ObservedObject var updater = DelayedUpdater()
     
-    func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void) {
-        // check the URL is OK, otherwise return with a failure
-        guard let url = URL(string: urlString) else {
-            completion(.failure(.badURL))
-            return
-        }
+    var body: some View {
+        Text("Value is \(updater.value)")
+    }
+}
 
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            // the task has completed – push our work back to the main thread
-            DispatchQueue.main.async {
-                if let data = data {
-                    // success: convert the data to a string and send it back
-                    let stringData = String(decoding: data, as: UTF8.self)
-                    completion(.success(stringData))
-                } else if error != nil {
-                    // any sort of network failure
-                    completion(.failure(.requestFailed))
-                } else {
-                    // this ought not to be possible, yet here we are
-                    completion(.failure(.unknown))
-                }
+class DelayedUpdater: ObservableObject {
+    var value = 0 {
+        // this works just like @Published does, but now we can add extra functionality within willSet like logging or calling another method
+        willSet {
+            objectWillChange.send()
+            print("Going up!")
+        }
+    }
+
+    init() {
+        for i in 1...10 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.value += 1
             }
-        }.resume()
+        }
     }
 }
 
