c0ccab4e8a26d35c805b8d3f5317e4f9c4dea098
Ryan Token <ryantoken13@gmail.com>
2021-09-19 21:27:31 -0500

    Reset the project back to normal

--- Project16-HotProspects/Project16-HotProspects/ContentView.swift
+++ Project16-HotProspects/Project16-HotProspects/ContentView.swift
@@ -6,19 +6,10 @@
 //
 
 import SwiftUI
-import SamplePackage
 
 struct ContentView: View {
-    let possibleNumbers = Array(1...60)
-    
-    var results: String {
-        let selected = possibleNumbers.random(7).sorted()
-        let strings = selected.map(String.init)
-        return strings.joined(separator: ", ")
-    }
-
     var body: some View {
-        Text(results)
+        Text("Hello")
     }
 }
 
