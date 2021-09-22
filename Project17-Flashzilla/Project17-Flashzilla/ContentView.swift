bf1d51e337e9fe929d4b5db2c252a1c1cea830db
Ryan Token <ryantoken13@gmail.com>
2021-09-22 09:07:56 -0500

    Began Project17 - Flashzilla

---
+++ Project17-Flashzilla/Project17-Flashzilla/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project17-Flashzilla
+//
+//  Created by Ryan Token on 9/22/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project17-Flashzilla/Project17-Flashzilla/Project17_FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project17_FlashzillaApp.swift
+//  Project17-Flashzilla
+//
+//  Created by Ryan Token on 9/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Project17_FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
