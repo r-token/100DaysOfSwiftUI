e8630b867d0691f95904a22586d34c876c3d6669
Ryan Token <ryantoken13@gmail.com>
2021-09-23 22:18:44 -0500

    Learned about triggering events with a Timer

--- Project17-Flashzilla/Project17-Flashzilla/ContentView.swift
+++ Project17-Flashzilla/Project17-Flashzilla/ContentView.swift
@@ -9,37 +9,20 @@ import SwiftUI
 import CoreHaptics
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            ZStack {
-                Rectangle()
-                    .fill(Color.blue)
-                    .frame(width: 300, height: 300)
-                    .onTapGesture {
-                        print("Rectangle tapped!")
-                    }
+    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common).autoconnect() // take out tolerance if you need the timer to be exact
+    @State private var counter = 0
 
-                Circle()
-                    .fill(Color.red)
-                    .frame(width: 300, height: 300)
-                    .contentShape(Rectangle())
-                    .onTapGesture {
-                        print("Circle tapped!")
-                    }
-                    // .allowsHitTesting(false)
+    var body: some View {
+        Text("Hello, World!")
+            .onReceive(timer) { time in
+                if counter == 5 {
+                    timer.upstream.connect().cancel() // stop the timer
+                } else {
+                    print("The time is now \(time)")
+                }
+
+                counter += 1
             }
-            
-            VStack {
-                Text("Hello")
-                Spacer().frame(height: 100)
-                Text("World")
-            }
-            .contentShape(Rectangle())
-            
-            .onTapGesture {
-                print("VStack tapped!")
-            }
-        }
     }
 }
 
