a7ae3cac6f0e227459fe2c9ed81728637a0a3d51
Ryan Token <ryantoken13@gmail.com>
2021-09-23 22:45:02 -0500

    Set the project back to normal before starting the actual Flashzilla app

--- Project17-Flashzilla/Project17-Flashzilla/ContentView.swift
+++ Project17-Flashzilla/Project17-Flashzilla/ContentView.swift
@@ -9,48 +9,8 @@ import SwiftUI
 import CoreHaptics
 
 struct ContentView: View {
-    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
-    
-    @State private var scale: CGFloat = 1
-    
-    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
-
     var body: some View {
-        VStack {
-            HStack {
-                if differentiateWithoutColor {
-                    Image(systemName: "checkmark.circle")
-                }
-
-                Text("Success")
-            }
-            .padding()
-            .background(differentiateWithoutColor ? Color.black : Color.green)
-            .foregroundColor(Color.white)
-            .clipShape(Capsule())
-            
-            Text("Scaling Text on Tap")
-                .scaleEffect(scale)
-                .onTapGesture {
-                    withOptionalAnimation {
-                        scale *= 1.5
-                    }
-                }
-            
-            Text("Ensuring We're Cool With Transparency")
-                .padding()
-                .background(reduceTransparency ? Color.black : Color.black.opacity(0.5))
-                .foregroundColor(Color.white)
-                .clipShape(Capsule())
-        }
-    }
-    
-    func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
-        if UIAccessibility.isReduceMotionEnabled {
-            return try body()
-        } else {
-            return try withAnimation(animation, body)
-        }
+        Text("Hay")
     }
 }
 
