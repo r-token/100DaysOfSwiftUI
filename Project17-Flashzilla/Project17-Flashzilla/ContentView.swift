04e7eb38553846ea3a91867f9fdc4cbfbf67c813
Ryan Token <ryantoken13@gmail.com>
2021-09-27 08:45:34 -0500

    Completed the basics for Project 17 - Flashzilla

--- Project17-Flashzilla/Project17-Flashzilla/ContentView.swift
+++ Project17-Flashzilla/Project17-Flashzilla/ContentView.swift
@@ -10,8 +10,10 @@ import CoreHaptics
 
 struct ContentView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.accessibilityEnabled) var accessibilityEnabled
     
-    @State private var cards = [Card](repeating: Card.example, count: 10)
+    @State private var showingEditScreen = false
+    @State private var cards = [Card]()
     @State private var timeRemaining = 100
     @State private var isActive = true
     
@@ -19,7 +21,7 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            Image("background")
+            Image(decorative: "background")
                 .resizable()
                 .scaledToFill()
                 .edgesIgnoringSafeArea(.all)
@@ -36,20 +38,57 @@ struct ContentView: View {
                             .opacity(0.75)
                     )
                 
-                if differentiateWithoutColor {
+                VStack {
+                    HStack {
+                        Spacer()
+
+                        Button(action: {
+                            showingEditScreen = true
+                        }) {
+                            Image(systemName: "plus.circle")
+                                .padding()
+                                .background(Color.black.opacity(0.7))
+                                .clipShape(Circle())
+                        }
+                    }
+
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                .font(.largeTitle)
+                .padding()
+                
+                if differentiateWithoutColor || accessibilityEnabled {
                     VStack {
                         Spacer()
 
                         HStack {
-                            Image(systemName: "xmark.circle")
-                                .padding()
-                                .background(Color.black.opacity(0.7))
-                                .clipShape(Circle())
+                            Button(action: {
+                                withAnimation {
+                                    removeCard(at: self.cards.count - 1)
+                                }
+                            }) {
+                                Image(systemName: "xmark.circle")
+                                    .padding()
+                                    .background(Color.black.opacity(0.7))
+                                    .clipShape(Circle())
+                            }
+                            .accessibility(label: Text("Wrong"))
+                            .accessibility(hint: Text("Mark your answer as being incorrect."))
                             Spacer()
-                            Image(systemName: "checkmark.circle")
-                                .padding()
-                                .background(Color.black.opacity(0.7))
-                                .clipShape(Circle())
+
+                            Button(action: {
+                                withAnimation {
+                                    removeCard(at: self.cards.count - 1)
+                                }
+                            }) {
+                                Image(systemName: "checkmark.circle")
+                                    .padding()
+                                    .background(Color.black.opacity(0.7))
+                                    .clipShape(Circle())
+                            }
+                            .accessibility(label: Text("Correct"))
+                            .accessibility(hint: Text("Mark your answer as being correct."))
                         }
                         .foregroundColor(.white)
                         .font(.largeTitle)
@@ -65,6 +104,8 @@ struct ContentView: View {
                             }
                         }
                         .stacked(at: index, in: cards.count)
+                        .allowsHitTesting(index == cards.count - 1)
+                        .accessibility(hidden: index < cards.count - 1)
                     }
                 }
                 .allowsHitTesting(timeRemaining > 0)
@@ -95,9 +136,16 @@ struct ContentView: View {
                 isActive = true
             }
         }
+        
+        .sheet(isPresented: $showingEditScreen, onDismiss: resetCards) {
+            EditCardsView()
+        }
+        
+        .onAppear(perform: resetCards)
     }
     
     func removeCard(at index: Int) {
+        guard index >= 0 else { return }
         cards.remove(at: index)
         
         if cards.isEmpty {
@@ -106,9 +154,17 @@ struct ContentView: View {
     }
     
     func resetCards() {
-        cards = [Card](repeating: Card.example, count: 10)
         timeRemaining = 100
         isActive = true
+        loadData()
+    }
+    
+    func loadData() {
+        if let data = UserDefaults.standard.data(forKey: "Cards") {
+            if let decoded = try? JSONDecoder().decode([Card].self, from: data) {
+                cards = decoded
+            }
+        }
     }
 }
 
