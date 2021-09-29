4f9394ab2c7500901db871ef0c6de525af715251
Ryan Token <ryantoken13@gmail.com>
2021-09-29 07:55:22 -0500

    Began Project 18 - LayoutAndGeometry

---
+++ Project18-LayoutAndGeometry/Project18-LayoutAndGeometry/Project18_LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project18_LayoutAndGeometryApp.swift
+//  Project18-LayoutAndGeometry
+//
+//  Created by Ryan Token on 9/29/21.
+//
+
+import SwiftUI
+
+@main
+struct Project18_LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
