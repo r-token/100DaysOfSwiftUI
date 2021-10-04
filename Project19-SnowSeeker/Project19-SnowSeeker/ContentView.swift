4b364c6c5e1c0e89314c1b88e05ca908b3c9a15f
Ryan Token <ryantoken13@gmail.com>
2021-10-04 08:41:45 -0500

    Cleared project in preparation for actual app build

--- Project19-SnowSeeker/Project19-SnowSeeker/ContentView.swift
+++ Project19-SnowSeeker/Project19-SnowSeeker/ContentView.swift
@@ -8,67 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var activeSheet: ActiveSheet?
-    
-    enum ActiveSheet: Identifiable {
-        case picker
-        
-        var id: UUID {
-            UUID()
-        }
-    }
-    
     var body: some View {
-        NavigationView {
-            Text("Hello, World!")
-            
-            .sheet(item: $activeSheet) { sheet in
-                SheetView()
-            }
-            
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: {
-                        activeSheet = ActiveSheet.picker
-                    }) {
-                        Text("Choose Me")
-                    }
-                }
-            }
-        }
+        Text("Hey")
     }
-}
-
-struct SheetView: View {
-    @Environment(\.horizontalSizeClass) var sizeClass
-    
-    var body: some View {
-        Group {
-            if sizeClass == .compact {
-                VStack {
-                    UserView()
-                }
-            } else {
-                HStack {
-                    UserView()
-                }
-            }
-        }
-    }
-}
-
-struct UserView: View {
-    var body: some View {
-        Group {
-            Text("Name: Paul")
-            Text("Country: England")
-            Text("Pets: Luna, Arya, and Toby")
-        }
-    }
-}
-
-struct User: Identifiable {
-    var id = "Taylor Swift"
 }
 
 struct ContentView_Previews: PreviewProvider {
